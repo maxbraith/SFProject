41bc9b6ebaee37670ec40ecddd2844f02c2c18c6
Yash Mohod <mohodyash@gmail.com>
2023-04-05 14:05:55 -0400

    update

---
+++ src/main/java/Library/Data/getAllBooked.java
@@ -0,0 +1,50 @@
+package Library.Data;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+// import Library.Book.Book;
+
+public class getAllBooked {
+    
+    public static void main(String[] args) throws IOException {
+        
+        // List<Book> Books = new LinkedList<Book>();
+
+        FileWriter myWriter = new FileWriter("bookData.csv");
+        myWriter.write("id,bookName,author,published");
+        myWriter.close();
+        
+
+
+
+
+        String line = "";  
+        String splitBy = ",";  
+        LinkedList<String[]> test = new LinkedList<String[]>();
+        BufferedReader br = new BufferedReader(new FileReader("bookData.csv")); 
+        while ((line = br.readLine()) != null){  
+            test.push(line.split(splitBy));
+        }  
+
+
+        for(String text: test.get(0)){
+            System.out.print(text);
+        }
+
+
+
+
+    }
+
+
+
+
+}
