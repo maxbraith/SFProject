b099a5d521c40b3799aacd9f830c501f8673e465
maxbraith <111820166+maxbraith@users.noreply.github.com>
2023-04-06 12:18:29 -0400

    Revert "Account tests branch"

--- src/main/java/Library/Users/librarian.java
+++ src/main/java/Library/Users/librarian.java
@@ -1,13 +1,7 @@
 package Library.Users;
 
 import java.util.ArrayList;
-
-import java.util.HashMap;
 import java.util.List;
-
-import Library.Library;
-
-
 
 import Library.Book.Book;
 
@@ -18,49 +12,12 @@ public class librarian extends parentUser{
         this.email = email;
         this.password = password;
         this.grade = grade;
+        this.accType = "Librarian";
     }
     
     public String accType(){
-        return null;
+        return accType;
     }
-
-
-
-    public HashMap<Integer, Integer> getRequests() {
-        return Library.requestList;
-    }
-
-    /**
-     * @post checks out a book if the book exists in the request list
-     * @param bookId - ID of book
-     * NOTE: Does not have a handler for if key does not exist
-     */
-    public void confirmRequestCheckout(int bookId){
-
-        int accountID = Library.requestList.get(bookId);
-        Book bookToCheckout = Library.books.get(bookId);
-
-        if (bookToCheckout == null) {
-            System.out.println("Error: Book with ID " + bookId + " not found in Library.books map");
-            return;
-        }
-
-        Library.books.remove(bookId);
-        Library.checkedOutBooks.put(bookId, bookToCheckout);
-        bookToCheckout.setTakenOut(true);
-
-        if (accountID != 0) {
-            bookToCheckout.setAccount(accountID);
-        }
-/* 
-        int accountID = Library.requestList.get(bookId);
-        Book bookToCheckout = Library.books.get(bookId);
-        Library.books.remove(bookId);
-        Library.checkedOutBooks.put(bookId, bookToCheckout);
-        bookToCheckout.setAccount(accountID);
-        bookToCheckout.setTakenOut(true);
-*/
-
 
     public parentUser makeAccount(int id, String email, String password, int grade, String accType){
         if(accType.equals("Student")){
@@ -86,6 +43,5 @@ public class librarian extends parentUser{
         }
         return null;
         
-
     }
 }
